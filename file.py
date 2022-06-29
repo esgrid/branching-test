2e48e1a828acafb62b1bfe462e0b3a446699c060
Esgrid <esikahall@gmail.com>
2022-06-29 13:22:57 +0100

    adding class File

--- file.py
+++ file.py
@@ -1,2 +1,16 @@
-def say_hello():
-    return "hello"
+class MyFile:
+    def __init__(self, _name, _size):
+        self.name = _name
+        self.size = _size
+    
+    def get_name(self):
+        return self.name
+    
+    def get_size(self):
+        return self.size
+
+    def set_name(self, new_name):
+        self.name = new_name
+    
+    def set_size(self, new_size):
+        self.size = new_size
